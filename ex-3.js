6f4ee49d2c060b4f2443981d00fc0887ce01781b
Phuree Limphornpugdee <phureelimphornpugdee@Phurees-MacBook-Air.local>
2024-01-10 03:26:23 +0700

    Exercise loop by Tar

--- ex-3.js
+++ ex-3.js
@@ -3,4 +3,10 @@ let numbers = [100, 20, 3, 1000];
 let minNumber;
 // Start coding here
 
+for (let i = 0; i < numbers.length; i = i +1)
+{
+if(minNumber < numbers[i])
+    {break;}
+else {minNumber = numbers[i]}};
+
 console.log(minNumber);
